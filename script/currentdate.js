02e5168bd558956087b8e9501edf449fe0b28046
salazarlj <salazarlj@gmail.com>
2022-04-23 17:30:25 -0600

    Last changes done

--- script/currentdate.js
+++ script/currentdate.js
@@ -1,23 +1,23 @@
-
-const authorName = "Leonardo J. Salazar"
-const autorCountry = "Nicaragua"
+const authorName = "Leonardo J. Salazar";
+const autorCountry = "Nicaragua";
 
 //Getting Author's name
-document.querySelector('#author').textContent = authorName;
-document.querySelector('#author2').textContent = authorName;
+document.querySelector("#author").textContent = authorName;
+document.querySelector("#author2").textContent = authorName;
 
 //Getting Course's name
-document.querySelector('#country').textContent = autorCountry;
+document.querySelector("#country").textContent = autorCountry;
 
 //Getting Current Year
-const d = new Date() ;
-const currentYears = document.querySelector('#currenYear');
+const d = new Date();
+const currentYears = document.querySelector("#currenYear");
 currentYears.textContent = d.getFullYear();
 
 //Getting Current Time
-const upateDates = document.querySelector('#updateDate');
-currentTime = `  ${d.getHours()}:${d.getMinutes()}:${d.getMilliseconds()}`
+const upateDates = document.querySelector("#updateDate");
+currentTime = `  ${d.getHours()}:${d.getMinutes()}:${d.getMilliseconds()}`;
 
 //Getting Current Date
-const selection = {month:'long', day:'numeric', year:'numeric'};
-upateDates.textContent = new Date().toLocaleDateString('en-US',selection) + currentTime;
+const selection = { month: "long", day: "numeric", year: "numeric" };
+upateDates.textContent =
+  new Date().toLocaleDateString("en-US", selection) + currentTime;
